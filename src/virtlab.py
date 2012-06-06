f0c79a5cabe94d57f17fcb4de924c85568afcfa9
Harri Savolainen <harri.savolainen@gmail.com>
2012-06-06 23:53:13 +0300

    Added GPL3 disclamers and the copy of the licence

--- src/virtlab.py
+++ src/virtlab.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 '''
+This file is part of Virtual Lab Manager.
+
+VM Lab Manager program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2012 Harri Savolainen
+
+@author: hsavolai, epelo
+@license: GPLv3
+
 Virtual Lab Manager Main
 '''
 from gtk import ListStore
