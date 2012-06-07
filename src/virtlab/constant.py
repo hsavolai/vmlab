d63dde94fad820ebc75c1bd952c633c182bb7f94
Harri Savolainen <hsavolai@redhat.com>
2012-06-08 00:30:57 +0300

    initial commit

---
+++ src/virtlab/constant.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''
+This file is part of Virtual Lab Manager.
+
+VM Lab Manager program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2012 Harri Savolainen
+
+Created on Jun 7, 2012
+
+@author: hsavolai
+@license: GPLv3
+'''
+
+
+EXCEPTION_LIBVIRT_001 = "LIBVIRT-001"
+EXCEPTION_LIBVIRT_001_DESC = "Failed to connect libvirtd"
+CONST_RUNNING = "RUNNING"
+CONST_STOPPED = "STOPPED"
