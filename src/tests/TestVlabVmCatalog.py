caf9019c6700bdb5be281fca93d5455f47e8fde5
Harri Savolainen <harri.savolainen@gmail.com>
2012-06-08 18:11:07 +0300

    Added testcases

--- src/tests/TestVlabVmCatalog.py
+++ src/tests/TestVlabVmCatalog.py
@@ -25,20 +25,21 @@ Created on Jun 7, 2012
 import unittest
 import mox
 from virtlab.virtual import VMLabException, LibVirtDao, VMCatalog
-from libvirt import virConnect
+from libvirt import virConnect, virDomain
 import virtlab.constant as c
 
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self.virConnect_mocker = mox.Mox()
+        self.mocker = mox.Mox()
+        self.virConnectMock = self.mocker.CreateMock(virConnect)
+        self.virDomainMock = self.mocker.CreateMock(virDomain)
 
     def tearDown(self):
         pass
 
     def setUpMVMock(self):
-        self.virConnectMock = self.virConnect_mocker.CreateMock(virConnect)
         static_stub = staticmethod(lambda *args, **kwargs: self.virConnectMock)
         LibVirtDao.get_libvirt = static_stub
 
@@ -52,11 +53,10 @@ class Test(unittest.TestCase):
         # TODO: Add check exception should include vme_id with correct value
         self.assertRaises(VMLabException, vmcatalog.get_conn)
 
-    def testGetSingleNotRunningVmInstance(self):
+    def testGetSingleStoppedVmInstance(self):
         self.setUpMVMock()
         self.virConnectMock.listDomainsID().AndReturn([])
-        vm_name = "TEST-VM"
-        #self.virConnectMock.listDomainsID().lookupByID(1).AndReturn()
+        vm_name = "TEST-VM-STOP"
         self.virConnectMock.listDefinedDomains().AndReturn([vm_name])
         mox.Replay(self.virConnectMock)
         vm_catalog = VMCatalog()
@@ -71,6 +71,58 @@ class Test(unittest.TestCase):
 
         self.assertEqual(c.CONST_STOPPED, vms[0].get_state().get_state_str(), \
                 "VM should be stopped / " + vms[0].get_state().get_state_str())
+
+    def testGetSingleRunningVmInstance(self):
+        self.setUpMVMock()
+        self.virConnectMock.listDomainsID().AndReturn([1])
+        vm_name = "TEST-VM-RUN"
+        self.virDomainMock.name().AndReturn(vm_name)
+        self.virConnectMock.lookupByID(1).AndReturn(self.virDomainMock)
+        self.virConnectMock.listDefinedDomains().AndReturn([])
+        mox.Replay(self.virConnectMock, self.virDomainMock)
+        vm_catalog = VMCatalog()
+        vm_catalog.refesh()
+        vms = vm_catalog.get_vms()
+        self.assertEqual(1, len(vms), "There should be only one vm! / "\
+                                                        + str(len(vms)))
+
+        self.assertEqual(vm_name, vms[0].get_name(), \
+                "VM name does not match! / " + vms[0].get_name())
+
+        self.assertEqual(c.CONST_RUNNING, vms[0].get_state().get_state_str(), \
+                "VM should be running / " + vms[0].get_state().get_state_str())
+
+    def testGetMixedVmInstances(self):
+        self.setUpMVMock()
+        # Setup running instance
+        self.virConnectMock.listDomainsID().AndReturn([1])
+        run_vm_name = "TEST-VM-RUN"
+        self.virDomainMock.name().AndReturn(run_vm_name)
+        self.virConnectMock.lookupByID(1).AndReturn(self.virDomainMock)
+
+        # Setup stopped instance
+        stop_vm_name = "TEST-VM-STOP"
+        self.virConnectMock.listDefinedDomains().AndReturn([stop_vm_name])
+
+        mox.Replay(self.virConnectMock, self.virDomainMock)
+        vm_catalog = VMCatalog()
+        vm_catalog.refesh()
+        vms = vm_catalog.get_vms()
+
+        self.assertEqual(2, len(vms), "There should be two vms! / "\
+                                                        + str(len(vms)))
+        self.assertEqual(stop_vm_name, vms[1].get_name(), \
+                "VM name does not match! / " \
+                                      + stop_vm_name + vms[1].get_name())
+
+        self.assertEqual(c.CONST_STOPPED, vms[1].get_state().get_state_str(), \
+                "VM should be stopped / " + vms[1].get_state().get_state_str())
+
+        self.assertEqual(run_vm_name, vms[0].get_name(), \
+                "VM name does not match! / " + vms[0].get_name())
+
+        self.assertEqual(c.CONST_RUNNING, vms[0].get_state().get_state_str(), \
+                "VM should be running / " + vms[0].get_state().get_state_str())
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
